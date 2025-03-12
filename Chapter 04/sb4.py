3e41738b3ea43da7b5c8e980bc31d301124d5ad8
sbCmonCmonCmon <ScottGBlomgren@Gmail.com>
2025-03-11 20:39:23 -0700

    add

--- Chapter 04/sb4.py
+++ Chapter 04/sb4.py
@@ -2,26 +2,31 @@ from teapot import *
 from vectors import *
 from draw_model import *
 
-
-def scale2(v):
-    return scale(2.0, v)
-
 original_triangles = load_triangles()
 
-def translate1left(v):
-    return add((-1,0,0), v)
+def scale_by(scalar):
+    def new_function(v):
+        return scale(scalar, v)
+    return new_function
+
+def translate_by(vector):
+    def new_function(v):
+        return add(vector, v)
+    return new_function
 
 def compose(f1, f2):
     def new_function(input):
         return f1(f2(input))
     return new_function
 
-scale_then_translate = compose(translate1left, scale2)
+scale_then_translate = compose(translate_by((-1,0,0)), scale_by(2))
 
-scaled_translated_triangles = [
-    [scale_then_translate(vertex) for vertex in triangle]
-    for triangle in original_triangles
-]
+def polygon_map(transformation, polygons):
+    return [
+        [transformation(vertex) for vertex in triangle]
+        for triangle in polygons
+    ]
 
-draw_model(scaled_translated_triangles)
+draw_model(polygon_map(scale_then_translate, original_triangles))
+
 
